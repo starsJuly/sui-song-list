d5e8fda5e9dbfd7685862b676bddc058c84c04ce
SuperChipsAhoy <176791594+SuperChipsAhoy@users.noreply.github.com>
2024-08-31 20:58:34 +0800

    chore: update theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       colors: {
         'badge-play': '#DA5D77',
         'bilibili': '#00AEEC',
-        'music-player-bg': '#655E66',
+        'music-player-bg': '#99C7F3',
         'label': '#000000',
         'secondary-label': '#4b5258',
         'secondary-bg': '#AFAFAF',
@@ -56,14 +56,43 @@ module.exports = {
 
         'vr-bg-light': '#e5feee',
         'vr-fg-light': '#53e383',
+        
+        'palette-1': '#E7E2E3', 
+        'palette-2': '#E97365', 
+        'palette-3': '#99C7F3', 
+        'palette-4': '#B3D6F6', 
+        'palette-5': '#F0F8FD', 
+        'palette-6': '#FEFEFE', 
+        'palette-7': '#70B9F2', 
+        'palette-8': '#7C9DB4', 
+        'palette-9': '#94B4CE', 
+        'palette-10': '#FBF2C6',
+
+        'label': '#7C9DB4',
+        'secondary-label': '#94B4CE',
+
+        'platinum': { 
+          DEFAULT: '#E7E2E3', 
+          100: '#322a2b', 
+          200: '#645457', 
+          300: '#947f83', 
+          400: '#beb1b4', 
+          500: '#e7e2e3', 
+          600: '#ece9e9', 
+          700: '#f1eeef', 
+          800: '#f6f4f4', 
+          900: '#faf9fa' 
+        }, 
+        'non_photo_blue': { DEFAULT: '#9BE0FC', 100: '#02394f', 200: '#05739e', 300: '#07aced', 400: '#4dc9f9', 500: '#9be0fc', 600: '#b0e7fd', 700: '#c4edfd', 800: '#d7f3fe', 900: '#ebf9fe' }, 'jordy_blue': { DEFAULT: '#99C7F3', 100: '#082947', 200: '#11528e', 300: '#197ad6', 400: '#53a2eb', 500: '#99c7f3', 600: '#afd3f6', 700: '#c3def8', 800: '#d7e9fa', 900: '#ebf4fd' }, 'uranian_blue': { DEFAULT: '#B3D6F6', 100: '#092b4c', 200: '#125798', 300: '#1b82e3', 400: '#66aced', 500: '#b3d6f6', 600: '#c1ddf8', 700: '#d0e6fa', 800: '#e0eefb', 900: '#eff7fd' }, 'alice_blue': { DEFAULT: '#F0F8FD', 100: '#0c3a57', 200: '#1874ae', 300: '#43a7e5', 400: '#9ad0f1', 500: '#f0f8fd', 600: '#f4fafe', 700: '#f7fbfe', 800: '#fafcfe', 900: '#fcfeff' }, 'white': { DEFAULT: '#FEFEFE', 100: '#333333', 200: '#666666', 300: '#999999', 400: '#cccccc', 500: '#fefefe', 600: '#ffffff', 700: '#ffffff', 800: '#ffffff', 900: '#ffffff' }, 'argentinian_blue': { DEFAULT: '#70B9F2', 100: '#062740', 200: '#0c4e81', 300: '#1275c1', 400: '#2e99ec', 500: '#70b9f2', 600: '#8bc7f4', 700: '#a8d5f7', 800: '#c5e3fa', 900: '#e2f1fc' }, 'air_superiority_blue': { DEFAULT: '#7C9DB4', 100: '#162027', 200: '#2d404e', 300: '#436075', 400: '#59809b', 500: '#7c9db4', 600: '#97b1c3', 700: '#b1c5d2', 800: '#cbd8e1', 900: '#e5ecf0' }, 'columbia_blue': { DEFAULT: '#C1D1DA', 100: '#1f2c34', 200: '#3e5867', 300: '#5d849b', 400: '#8fabbc', 500: '#c1d1da', 600: '#cfdbe2', 700: '#dbe4e9', 800: '#e7edf0', 900: '#f3f6f8' }, 'lemon_chiffon': { DEFAULT: '#FBF2C6', 100: '#544706', 200: '#a88e0c', 300: '#efcc1e', 400: '#f5df72', 500: '#fbf2c6', 600: '#fcf5d1', 700: '#fdf7dd', 800: '#fdfae8', 900: '#fefcf4' }
       },
     },
     fontSize: {
       'header': '3rem',
       'title': '2rem',
       'subtitle': '1.2rem',
-      'base': '1rem',
+      'base': '0.9rem',
       'sm': '0.8rem',
+      'xs': '0.7rem'
     }
   },
   plugins: [],
