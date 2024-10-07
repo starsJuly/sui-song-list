595d478b47f9c837789878480687b3a4afda3cf6
SuperChipsAhoy <176791594+SuperChipsAhoy@users.noreply.github.com>
2024-10-07 01:19:36 -0700

    fix: copy color presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -125,7 +125,34 @@ module.exports = {
 
         'vr-bg': '#1D3222',
         'vr-fg': '#6BE98C',
+        
+        // generated from #87EAFF
+        'oen-blue': '#87EAFF',
+        'oen-color-1': "#e1fdff",
+        'oen-color-2': "#ccf8ff",
+        'oen-color-3': "#9ceeff",
+        'oen-color-4': "#68e4fe",
+        'oen-color-5': "#43dbfd",
+        'oen-color-6': "#2fd6fd",
+        'oen-color-7': "#1dd4fe",
+        'oen-color-8': "#00bbe3",
+        'oen-color-9': "#00a7cb",
+        'oen-color-10': "#0090b3",
 
+        // generated from #DA5D77
+        'oen-red': '#DA5D77',
+        'oen-red-2': '#E97365',
+        'oen-color-11': "#ffeaf1",
+        'oen-color-12': "#fbd6df",
+        'oen-color-13': "#efadbb",
+        'oen-color-14': "#e38095",
+        'oen-color-15': "#da5b75",
+        'oen-color-16': "#d44361",
+        'oen-color-17': "#d23657",
+        'oen-color-18': "#bb2747",
+        'oen-color-19': "#a71f3f",
+        'oen-color-20': "#941335",
+        
         'label': '#F0D59E',
         'secondary-label': '#EDC69A',
         'tertiary-label': '#733E2E',
